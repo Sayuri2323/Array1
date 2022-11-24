f0f7f313ec74a06949ea5243cca0b20484949ee0
Sayuri2323 <117921120+Sayuri2323@users.noreply.github.com>
2022-11-24 17:56:41 +0100

    array1 esercizio

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] carBrands=new String[]
+                {"Bugatti", "Lamborghini", "Ferrari", "Bentley", "Tesla", "Hummer"};
+        System.out.println(carBrands.length);
+        System.out.println(carBrands[2]);
+
+        int[] primeNumbers=new int[6];
+        primeNumbers[0]=2;
+        primeNumbers[1]=3;
+        primeNumbers[2]=5;
+        primeNumbers[3]=7;
+        primeNumbers[4]=11;
+        primeNumbers[5]=13;
+
+        System.out.println(Arrays.toString(primeNumbers));
+    }
+}
